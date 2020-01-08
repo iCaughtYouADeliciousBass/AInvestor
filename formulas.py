edce927ff9d1e908c65f27430db5b0aac808c892
Michael Judd <michael.judd@icsc.co.nz>
2020-01-09 12:09:55 +1300

    Calculation for RSI added, test at home

--- formulas.py
+++ formulas.py
@@ -2,7 +2,7 @@ def momentum(data, interval, count):
     mom = 0
     try:
         mom = (data[count-1].c - data[0].c) / data[0].c
-        print('momentum = {} over the last {} {}'.format(mom, interval, count))
+        print('momentum = {} over the last {} {}s'.format(mom, count, interval))
 
     except:
         print('Momentum calculation failed -- Interval Out of Range')
@@ -10,5 +10,53 @@ def momentum(data, interval, count):
     finally:
         return mom
 
-def RSI():
-    pass
+def RSI(data, count, interval, average):
+    close_array = []
+    change_array = ['0']
+    upward_array = []
+    downward_array = []
+    upward_average = []
+    downward_average = []
+    relative_strength = []
+    RSI = []
+    for i in range(count):
+        close_array.append(data[count].c)
+
+    for i in len(close_array)-2:
+        change = close_array[i+1] - close_array[i]
+        change_array.append(change)
+        if change >= 0:
+            upward_array.append(abs(change))
+            downward_array.append('0')
+        if change <= 0:
+            downward_array.append(abs(change))
+            upward_array.append('0')
+
+    temp_up_avg = upward_array[average-1:]
+    temp_down_avg = downward_array[average-1:]
+    length_of_movement = len(upward_array)-average+1
+    if length_of_movement > 0:
+        for i in range(length_of_movement):
+            if i == 0:
+                upward_average.append(sum(upward_array[i:average+i]))
+            else:
+                upward_average.append((upward_average[i - 1] * (average - 1) + upward_array[average + i]) / average)
+
+        for i in range(length_of_movement1):
+            if i==0:
+                downward_average.append(sum(downward_array[i:average+i]))
+            else:
+                downward_average.append((downward_average[i - 1] * (average - 1) + downward_array[average + i]) / average)
+
+        for i in len(upward_average):
+            relative_strength.append(upward_average[i] / downward_average[i])
+
+        for i in len(relative_strength):
+            RSI.append(100-100/(relative_strength[i]+1))
+
+        return RSI
+
+    else:
+        print('Not Enough Points in Data Array to Generate RSI -- Points: {}, Average: {}, Length: {}'.format(count, average, length_of_movement))
+        return 0
+
