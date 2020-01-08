510f2e4cb898b0cd04c5451eb1d966f69c090f7e
Michael Judd <michael.judd@icsc.co.nz>
2020-01-09 10:05:09 +1300

    Updated momentum to use interval

--- formulas.py
+++ formulas.py
@@ -1,4 +1,4 @@
 def momentum(data, interval, count):
-    mom = (data[-1].c - data[0].c) / data[0].c
+    mom = (data[count-1].c - data[0].c) / data[0].c
     print('momentum = {} over the last {} {}'.format(mom, interval, count))
     return mom
