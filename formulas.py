c2b036aea413889abc453584d75c0b23bb0be2bb
Michael Judd <michael.judd@icsc.co.nz>
2020-01-09 10:42:50 +1300

    Added RSI

--- formulas.py
+++ formulas.py
@@ -1,4 +1,14 @@
 def momentum(data, interval, count):
-    mom = (data[count-1].c - data[0].c) / data[0].c
-    print('momentum = {} over the last {} {}'.format(mom, interval, count))
-    return mom
+    mom = 0
+    try:
+        mom = (data[count-1].c - data[0].c) / data[0].c
+        print('momentum = {} over the last {} {}'.format(mom, interval, count))
+
+    except:
+        print('Momentum calculation failed -- Interval Out of Range')
+
+    finally:
+        return mom
+
+def RSI():
+    pass
